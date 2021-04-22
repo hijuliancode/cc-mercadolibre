d5fcbbe3c9a36b4d78ff2d25d6e369465bf13fc3
hijuliancode <hijuliansosa@protonmail.com>
2021-04-22 16:30:54 -0500

    STYLES: Add SASS support, add ITCSS folders & base files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { DemoComponent } from './components/shared/DemoComponent'
+import './scss/main.scss'
+
 function App() {
   return (
     <div className="App">
       App.js
+      <hr/>
+
+      <DemoComponent />
     </div>
   );
 }
---
+++ src/components/shared/DemoComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const DemoComponent = () => {
+  return (
+    <div>
+      <h1 className="demo-component">Hello World</h1>
+    </div>
+  )
+}
