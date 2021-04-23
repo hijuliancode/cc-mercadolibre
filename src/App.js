6935d9f5d59837ff7596d51ad5cf02b1dce993c9
hijuliancode <hijuliansosa@protonmail.com>
2021-04-23 00:05:00 -0500

    STYLES: VDS, Grid System, fix Container Variables

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       App.js
+
+      <div className="grid xxs-2-columns lg-4-columns">
+        <p>hola</p>
+        <p>mundo</p>
+        <p>como</p>
+        <p>vamos?</p>
+      </div>
       <hr/>
 
       <DemoComponent />
