e87b25b2e804e1ad60e51dd2347e04da036abacc
hijuliancode <hijuliansosa@protonmail.com>
2021-04-26 23:23:39 -0500

    UI : Markup: Product Detail

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import { LayoutInternal } from '../Layouts/LayoutInternal'
 import { DemoComponent } from '../../components/shared/DemoComponent'
 import { useFetchItem } from '../../hooks'
+import { Alert, Box, CardResult, Container, Image } from '../../components'
+import Button from '../../components/Button'
 
 
 export const ProductDetail = () => {
@@ -14,12 +16,41 @@ export const ProductDetail = () => {
 
   return (
     <LayoutInternal>
-      {
-        !loading && (
-          <h1>Título {product.title}: {id}</h1> 
-        )
-      }
-     <DemoComponent />
+      <Container className="d-flex">
+        <Box className="radius-xm p-6">
+          {
+            loading ?
+            <Alert>Cargando..</Alert>
+            : (
+              <div className="product">
+                <div className="grid-lg lg-columns-8-4">
+                  <div className="product-gallery">
+                    <Image
+                      className="radius-xm"
+                      alt={product.title}
+                      height={480}
+                      local={false}
+                      src={product.thumbnail}
+                      width={480} />
+                  </div>
+                  <div className="product-info">
+                    <span className="product-status">Nuevo - 254 Vendidos</span>
+                    <h2 className="product-name">{product.title}</h2>
+                    <span className="product-price">$1.980 <sup>00</sup></span>
+                    <div className="product-btn-buy">
+                      <Button className="btn-primary">Comprar</Button>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h4 className="product-title">Descripcion del producto</h4>
+                  <p className="product-desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis eveniet labore ducimus animi enim rem! Perspiciatis ullam a iusto obcaecati ducimus aperiam, vitae rerum ratione pariatur, sit expedita facere ipsam?</p>
+                </div>
+              </div>
+            )
+          }
+        </Box>
+      </Container>
     </LayoutInternal>
   )
 }
