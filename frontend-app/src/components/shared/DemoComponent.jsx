13df59e318967be0614f7dc67166132008dc6713
hijuliancode <hijuliansosa@protonmail.com>
2021-04-29 21:18:53 -0500

    update .gitignore, remove styled from demo component

--- frontend-app/src/components/shared/DemoComponent.jsx
+++ frontend-app/src/components/shared/DemoComponent.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-
-const Nav = styled.nav`
-  position: absolute;
-  top: 0;
-`
 
 export const DemoComponent = () => {
   console.log('DemoComponent.jsx')
   return (
-    <Nav>
+    <nav>
       <Link to="/">Home</Link>
       <Link to="/items/?search=Hola Mundo">Query Search</Link>
       <Link to="/items/MLA719793747">Items:ID</Link>
-    </Nav>
+    </nav>
   )
 }
