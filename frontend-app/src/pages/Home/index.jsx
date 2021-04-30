81c3ec67ef35463add7515271fab4b5646c00614
hijuliancode <hijuliansosa@protonmail.com>
2021-04-30 04:29:06 -0500

    fix: Layout Home console Alerts

--- frontend-app/src/pages/Home/index.jsx
+++ frontend-app/src/pages/Home/index.jsx
@@ -4,14 +4,16 @@ import { Image, SearchBar } from '../../components';
 
 const Home = () => (
   <LayoutHome>
-    <Image
-      className="home__logo"
-      src="design/logos/log-mla.png"
-      alt="Mercadolibre"
-      height={70}
-      width={268}
-    />
-    <SearchBar className="mb-10" type="rounded" voice />
+    <>
+      <Image
+        className="home__logo"
+        src="design/logos/log-mla.png"
+        alt="Mercadolibre"
+        height={70}
+        width={268}
+      />
+      <SearchBar className="mb-10" type="rounded" voice />
+    </>
   </LayoutHome>
 );
 
